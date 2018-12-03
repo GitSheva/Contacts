f4aa9fcf9746af9337a4d12cf3908d1a50f54471
Ryabko <Artem.Ryabko@colliers.com>
2018-12-03 15:25:43 -0800

    remove version console.log

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-console.log(React.version);
+
 class App extends Component {
   render() {
     return (
