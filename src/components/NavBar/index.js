f330a20727c28acee17ab9388a1d60509edadf44
Your Name <aryabko@gmail.com>
2018-12-09 08:25:17 -0800

    renamed

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -5,25 +5,13 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
 
-const styles = {
-  root: {
-    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-    borderRadius: 3,
-    border: 0,
-    color: "white",
-    height: 48,
-    padding: "0 30px",
-    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)"
-  }
-};
-
 const NavBar = () => {
   return (
     <div>
       <AppBar position="static" color="default">
         <Toolbar>
           <Typography variant="title" color="inherit">
-            Travelex
+            Contacts
           </Typography>
           <Button component={Link} to="/login" color="inherit">
             Login
