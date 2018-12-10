940b795e2984361e35ba71df53f9b40a3b0a00cc
Your Name <aryabko@gmail.com>
2018-12-09 16:21:13 -0800

    changes

--- src/components/ContactTable/index.js
+++ src/components/ContactTable/index.js
@@ -20,9 +20,9 @@ import FilterListIcon from "@material-ui/icons/FilterList";
 import { lighten } from "@material-ui/core/styles/colorManipulator";
 
 let counter = 0;
-function createData(name, calories, fat, carbs, protein) {
+function createData(name, email, phone, job) {
   counter += 1;
-  return { id: counter, name, calories, fat, carbs, protein };
+  return { id: counter, name, email, phone, job };
 }
 
 function desc(a, b, orderBy) {
@@ -56,12 +56,16 @@ const rows = [
     id: "name",
     numeric: false,
     disablePadding: true,
-    label: "Dessert (100g serving)"
+    label: "Name"
   },
-  { id: "calories", numeric: true, disablePadding: false, label: "Calories" },
-  { id: "fat", numeric: true, disablePadding: false, label: "Fat (g)" },
-  { id: "carbs", numeric: true, disablePadding: false, label: "Carbs (g)" },
-  { id: "protein", numeric: true, disablePadding: false, label: "Protein (g)" }
+  { id: "email", numeric: false, disablePadding: true, label: "Email" },
+  { id: "phone", numeric: false, disablePadding: true, label: "Phone number" },
+  {
+    id: "job",
+    numeric: false,
+    disablePadding: true,
+    label: "Job title & company"
+  }
 ];
 
 class EnhancedTableHead extends React.Component {
@@ -168,7 +172,7 @@ let EnhancedTableToolbar = props => {
           </Typography>
         ) : (
           <Typography variant="h6" id="tableTitle">
-            Nutrition
+            Contact List
           </Typography>
         )}
       </div>
@@ -218,19 +222,84 @@ class EnhancedTable extends React.Component {
     orderBy: "calories",
     selected: [],
     data: [
-      createData("Cupcake", 305, 3.7, 67, 4.3),
-      createData("Donut", 452, 25.0, 51, 4.9),
-      createData("Eclair", 262, 16.0, 24, 6.0),
-      createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-      createData("Gingerbread", 356, 16.0, 49, 3.9),
-      createData("Honeycomb", 408, 3.2, 87, 6.5),
-      createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-      createData("Jelly Bean", 375, 0.0, 94, 0.0),
-      createData("KitKat", 518, 26.0, 65, 7.0),
-      createData("Lollipop", 392, 0.2, 98, 0.0),
-      createData("Marshmallow", 318, 0, 81, 2.0),
-      createData("Nougat", 360, 19.0, 9, 37.0),
-      createData("Oreo", 437, 18.0, 63, 4.0)
+      createData(
+        "Diego Maradona",
+        "diego@hotmail.com",
+        49856325,
+        "Professional football player"
+      ),
+      createData(
+        "Diego Maradona",
+        "diego@hotmail.com",
+        49856325,
+        "Professional football player"
+      ),
+      createData(
+        "Diego Maradona",
+        "diego@hotmail.com",
+        49856325,
+        "Professional football player"
+      ),
+      createData(
+        "Diego Maradona",
+        "diego@hotmail.com",
+        49856325,
+        "Professional football player"
+      ),
+      createData(
+        "Diego Maradona",
+        "diego@hotmail.com",
+        49856325,
+        "Professional football player"
+      ),
+      createData(
+        "Diego Maradona",
+        "diego@hotmail.com",
+        49856325,
+        "Professional football player"
+      ),
+      createData(
+        "Diego Maradona",
+        "diego@hotmail.com",
+        49856325,
+        "Professional football player"
+      ),
+      createData(
+        "Diego Maradona",
+        "diego@hotmail.com",
+        49856325,
+        "Professional football player"
+      ),
+      createData(
+        "Diego Maradona",
+        "diego@hotmail.com",
+        49856325,
+        "Professional football player"
+      ),
+      createData(
+        "Diego Maradona",
+        "diego@hotmail.com",
+        49856325,
+        "Professional football player"
+      ),
+      createData(
+        "Diego Maradona",
+        "diego@hotmail.com",
+        49856325,
+        "Professional football player"
+      ),
+      createData(
+        "Diego Maradona",
+        "diego@hotmail.com",
+        49856325,
+        "Professional football player"
+      ),
+      createData(
+        "Diego Maradona",
+        "diego@hotmail.com",
+        49856325,
+        "Professional football player"
+      )
     ],
     page: 0,
     rowsPerPage: 5
@@ -326,10 +395,15 @@ class EnhancedTable extends React.Component {
                       <TableCell component="th" scope="row" padding="none">
                         {n.name}
                       </TableCell>
-                      <TableCell numeric>{n.calories}</TableCell>
-                      <TableCell numeric>{n.fat}</TableCell>
-                      <TableCell numeric>{n.carbs}</TableCell>
-                      <TableCell numeric>{n.protein}</TableCell>
+                      <TableCell component="th" scope="row" padding="none">
+                        {n.email}
+                      </TableCell>
+                      <TableCell component="th" scope="row" padding="none">
+                        {n.phone}
+                      </TableCell>
+                      <TableCell component="th" scope="row" padding="none">
+                        {n.job}
+                      </TableCell>
                     </TableRow>
                   );
                 })}
