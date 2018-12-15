2853b4ef55c32a69f3d98589e480867ae43ba5af
Your Name <aryabko@gmail.com>
2018-12-15 15:47:20 -0800

    done with hooks

--- src/components/testc/Textc.js
+++ src/components/testc/Textc.js
@@ -1,21 +1,14 @@
 import React, { useState, useEffect } from "react";
 
 const Textc = ({ selectValue }) => {
-  //Here selectValue is coming ok....
-  console.log("selectValue: " + selectValue);
-
-  //Init value with selectValue coming from Select onChange. Looks like doesn't work
-  //If I initialize value with any static string or number, it works. I can see value in text box. But not from props.
   let [value, setValue] = useState(selectValue);
-  //Here value is not updated in state
-  console.log("stateValue: " + value);
 
-  useEffect(
-    () => {
-      setValue(selectValue);
-    },
-    [selectValue],
+  useEffect(() => {
+    setValue(selectValue);
+  },
+  [selectValue],
   );
+
   const handleOnClick = event => {
     alert(value);
     event.preventDefault();
