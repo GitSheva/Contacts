7c8cc044160e73c9a66ea8ceac9ac8fcea316d59
mryabko <maxryabko@gmail.com>
2018-12-18 16:15:37 -0800

    added form

--- src/components/AddContact/index.js
+++ src/components/AddContact/index.js
@@ -1,8 +1,67 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import TextField from '@material-ui/core/TextField';
 
 class AddContact extends Component {
+  state = {
+    user: {
+      name: '',
+      email: '',
+      phone: '',
+      website: ''
+    }
+  };
+
+  handleChange = name => ({ target: { value } }) => {
+    this.setState({
+      user: {
+        ...this.state.user,
+        [name]: value
+      }
+    });
+  };
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.user.name);
+    event.preventDefault();
+  }
+
   render() {
-    return <h4>Add Contact</h4>;
+    const {
+      user: { name, email, phone, website }
+    } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <TextField
+          label="Name"
+          value={name}
+          onChange={this.handleChange('name')}
+          margin="normal"
+        />
+        <br />
+        <TextField
+          label="Email"
+          value={email}
+          onChange={this.handleChange('email')}
+          margin="normal"
+        />
+        <br />
+        <TextField
+          label="Phone"
+          value={phone}
+          onChange={this.handleChange('phone')}
+          margin="normal"
+        />
+        <br />
+        <TextField
+          label="Website"
+          value={website}
+          onChange={this.handleChange('website')}
+          margin="normal"
+        />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    );
   }
 }
 
